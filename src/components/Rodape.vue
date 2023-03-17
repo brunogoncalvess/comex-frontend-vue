ad71e20224dddfc1e644f0010ac2dbe3046134a7
Bruno <bruno.goncalves@onebox.one>
2023-03-17 10:40:40 -0300

    cria componente de rodapé

---
+++ src/components/Rodape.vue
@@ -0,0 +1,14 @@
+<template>
+  <footer class="footer">
+    <div class="container">
+      <p>&copy; 2023 My Awesome Website</p>
+    </div>
+  </footer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: 'Rodape'
+})
+</script>
